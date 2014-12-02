0e918cfbb04eb834b70bfa3f1a94e7aa183af4a3
nathan lopez <cherrygal2011@hotmail.com>
2014-12-02 15:01:32 -0800

    video 18

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -13,22 +13,25 @@ game.PlayerEntity = me.Entity.extend({
             }]);
 
         this.renderable.addAnimation("idle", [3]);
-        this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
+        this.renderable.addAnimation("smallWalk", [38, 39, 40, 41, 42, 43], 80);
 
         this.renderable.setCurrentAnimation("idle");
 
         this.body.setVelocity(5, 20);
-   },
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+    },
     update: function(delta) {
         if (me.input.isKeyPressed("right")) {
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+        } else if (me.input.isKeyPressed("left")) {
+            this.body.vel.x -= this.body.accel.x * me.timer.tick;
 
         } else {
             this.body.vel.x = 0;
         }
-        
+
         this.body.update(delta);
-        me.collision.check(this, true, this.collideHandler.bind(this))
+        me.collision.check(this, true, this.collideHandler.bind(this));
 
         if (this.body.vel.x !== 0) {
             if (!this.renderable.isCurrentAnimation("smallWalk")) {
@@ -41,25 +44,24 @@ game.PlayerEntity = me.Entity.extend({
         this._super(me.Entity, "update", [delta]);
         return true;
     },
-    
-    collideHandler: function(response){
-        
+    collideHandler: function(response) {
+
     }
 
 });
 
 game.LevelTrigger = me.Entity.extend({
-    init: function(x, y, settings){
+    init: function(x, y, settings) {
         this._super(me.Entity, 'init', [x, y, settings]);
         this.body.onCollision = this.onCollision.bind(this);
         this.level = settings.level;
     },
-    
-    onCollision: function(){
+    onCollision: function() {
         this.body.setCollisionMask(me.collision.types.NO_OBJECT);
         me.levelDirector.loadLevel(this.level);
-        
+        me.state.current().resetPlayer();
+
     }
-    
+
 });
 
--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -8,8 +8,7 @@ game.PlayScreen = me.ScreenObject.extend({
                 
                 me.levelDirector.loadLevel("mtgz01");
                 
-                var player = me.pool.pull("mario", 0, 420, {});
-                me.game.world.addChild(player, 7);
+                this.resetPlayer(0, 400);
                 
                 me.input.bindKey(me.input.KEY.RIGHT, "right");
                  me.input.bindKey(me.input.KEY.LEFT, "left");
@@ -25,7 +24,12 @@ game.PlayScreen = me.ScreenObject.extend({
 	onDestroyEvent: function() {
 		// remove the HUD from the game world
 		me.game.world.removeChild(this.HUD);
-	}
+	},
+        
+        resetPlayer: function() {
+             var player = me.pool.pull("mario", 0, 420, {});
+                me.game.world.addChild(player, 5);
+        }
         
         
         
